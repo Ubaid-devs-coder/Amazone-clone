e4577ed1579db7bf992927aa554e6023d2b5b1e2
Rajpoot <ubaid11357>
2025-08-20 15:30:35 +0500

    use products.js files

--- scripts/Amazon.js
+++ scripts/Amazon.js
@@ -1,31 +1,6 @@
 // Save the data
 
-const products = [{
-  img: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: '4.5',
-    count: 87
-  },
-  priceCents: 1090  // 1 dollar = 90 cents
-}, {
-  img: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: '4',
-    count: 127
-  },
-  priceCents: 2095
-}, {
-  img: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: '4.5',
-    count: 56
-  },
-  priceCents: 799
-}]
-
+  
 // Generate the Html with loop
 
 let productsHTML = ''
@@ -35,7 +10,7 @@ products.forEach((product) => {
    <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="${product.img}">
+              src="${product.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
