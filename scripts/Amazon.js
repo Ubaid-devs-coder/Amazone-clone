92c4633655056d8f9f034a668d98856197a594e7
Rajpoot <ubaid11357>
2025-08-21 11:45:14 +0500

    import products from products.js

--- scripts/Amazon.js
+++ scripts/Amazon.js
@@ -1,4 +1,5 @@
 import {cart} from '../data/cart.js'
+import {products} from '../data/products.js'
 
 // Save the data (products.js => for products objects)
    // save data (cart.js => for cart objects)
