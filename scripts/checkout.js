999c3526e7873b87676eb78a2d25d5f0ef98b2e3
Rajpoot <ubaid11357>
2025-08-22 16:49:11 +0500

    make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,10 @@
-import { cart, removeFromCart } from '../data/cart.js'
+import { cart, removeFromCart, updateDeliveryOption } from '../data/cart.js'
 import { products } from '../data/products.js'
 import { formatCurrency } from './utils/money.js'
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js'
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
 import { deliveryOptions } from '../data/dileveryOptions.js'
+
 
 
 hello()
@@ -103,7 +104,9 @@ function deliveryOptionsHtml(matchingProduct, cartItem) {
 
 
     html += `
-                <div class="delivery-option">
+                <div class="delivery-option js-delivery-option" 
+                  data-product-id="${matchingProduct.id}"
+                  data-delivery-option-id="${deliveryOption.id}">
                   <input type="radio"
                   ${isChecked ? 'checked' : ''}
                     class="delivery-option-input"
@@ -135,3 +138,13 @@ document.querySelectorAll('.js-delete-link')
       container.remove()
     })
   })
+
+  document.querySelectorAll('.js-delivery-option')
+    .forEach((element) => {
+      element.addEventListener('click', () => {
+       /* const productId = element.dataset.productId
+        const deliveryOptionId = element.dataset.deliveryOptionId */
+        const {productId, deliveryOptionId} = element.dataset
+        updateDeliveryOption(productId, deliveryOptionId)
+      })
+    })
