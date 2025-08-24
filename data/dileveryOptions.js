03955e77279d43cd0dcb91a7f440dac939e3951c
Rajpoot <ubaid11357>
2025-08-24 22:31:14 +0500

    Save data and generate HTML for parents Summary

--- data/dileveryOptions.js
+++ data/dileveryOptions.js
@@ -2,12 +2,25 @@ export const deliveryOptions = [{
   id: '1',
   deliverDays: 7,
   priceCents: 0
-} , {
+}, {
   id: '2',
   deliverDays: 3,
   priceCents: 499
-} , {
+}, {
   id: '3',
   deliverDays: 1,
   priceCents: 999
 }]
+
+export function getDilveryOption(deliveryOptionId) {
+  let deliveryOption
+
+
+  deliveryOptions.forEach((option) => {
+    if (deliveryOptionId === option.id) {
+      deliveryOption = option
+    }
+  })
+
+  return deliveryOption || deliveryOptions[0]
+}
