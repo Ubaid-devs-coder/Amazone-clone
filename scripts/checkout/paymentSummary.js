5944f77db2f8337234b678ff3cb01e2257a54046
Rajpoot <ubaid11357>
2025-08-28 15:34:54 +0500

    create order using the backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from '../../data/cart.js'
 import { getProduct } from '../../data/products.js';
 import { deliveryOptions, getDilveryOption } from '../../data/dileveryOptions.js';
 import { formatCurrency } from '../utils/money.js'
+import { addOrder } from '../../data/orders.js';
 
 export function renderPaymentSummary() {
   let productPriceCents = 0
@@ -50,11 +51,34 @@ export function renderPaymentSummary() {
           <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary js-place-order">
           Place your order
         </button>
   `
 
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML
 
+  document.querySelector('.js-place-order')
+    .addEventListener('click', async () => {
+      try {
+        const response = await fetch('https://supersimplebackend.dev/orders', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            cart: cart
+          })
+        })
+
+        const order = await response.json()
+        addOrder(order)
+
+      } catch (error) {
+        console.log('unexpected error. Pleae try again later.')
+      }
+
+      // window.location change the browser address when we click on btn
+      window.location.href = 'orders.html'
+    })
 }
